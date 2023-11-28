0e07ac9869979953bd66f2e5821fa072fac5882a
Ivan <djohnmarkivan@yahoo.com>
2023-11-28 09:41:47 +0800

    dev: templates online feature

--- components/EscalationTemplate.tsx
+++ components/EscalationTemplate.tsx
@@ -32,168 +32,192 @@ import { ContextMenu, ContextMenuTrigger } from "@/components/ui/context-menu"
 
 import { TemplateContextContent } from '@/components/TemplateItemContextMenu'
 
-import {storageKeys} from '@/lib/data'
+import { storageKeys } from '@/lib/data'
 import { Separator } from "@/components/ui/separator"
+import * as fsClient from "@/lib/firebase/clientApp"
+import { UserAuth } from "@/app/context/AuthContext"
 
 const storageKey = storageKeys.templates;
 
 export default function EscalationTemplateManager() {
-    const [selectedTemplate, setSelectedTemplate] = useState<IEscalationTemplate>(new EscalationTemplate('', '', ''))
-    const [templates, setTemplates] = useState<IEscalationTemplate[]>([])
-    const [selectedIsNew, setSelectedIsNew] = useState<boolean>(false);
-    const [open, setOpen] = useState(false);
-    const [loading, setLoading] = useState(true);
-  
-    const PersistTemplates = debounce(() => {
+  const [selectedTemplate, setSelectedTemplate] = useState<IEscalationTemplate>(new EscalationTemplate('', '', ''))
+  const [templates, setTemplates] = useState<IEscalationTemplate[]>([])
+  const [selectedIsNew, setSelectedIsNew] = useState<boolean>(false);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { user } = UserAuth();
+
+
+  const PersistTemplates = debounce(() => {
+    if (fsClient.IsLoggedIn()) {
+      fsClient.SaveTemplates(templates)
+    } else {
       localStorage.setItem(storageKey, JSON.stringify(templates));
-    }, 400);
-  
-    useEffect(() => {
+    }
+  }, 400);
+
+  useEffect(() => {
+    let cancel = false;
+    if (fsClient.IsLoggedIn()) {
+      (async () => {
+        let templates = await fsClient.GetTemplates()
+        if (!cancel) {
+          setTemplates(templates);
+        }
+        setLoading(false);
+        return;
+      })()
+    } else {
       let result = localStorage.getItem(storageKey);
-      if(result != null) {
+      if (result != null) {
         try {
-          let t:EscalationTemplate[] = JSON.parse(result);
-          setTemplates(t);
+          let t: EscalationTemplate[] = JSON.parse(result);
+          if (!cancel)
+            setTemplates(t);
         } catch {
   
         }
       }
       setLoading(false);
-    }, [])
-  
-    useEffect(() => {
-      if(loading) return;
-      PersistTemplates();
-    }, [templates])
-  
-    useEffect(() => {
-      if( templates.every( t => Boolean(t.guid?.trim()) )) return;
-      setTemplates( curr => {
-        const d = [...curr];
-        for(const template of d) {
-          template.guid = uuidv4();
-        }
-        return d;
-      });
-    }, [templates])
-  
-    useEffect(() => {
-      if(selectedTemplate.guid?.trim()) return;
-      setSelectedTemplate( curr => {
-        const d = Object.assign({}, curr);
-        SetGuid(d);
-        return d;
-      });
-    }, [selectedTemplate])
-  
-    useEffect(() => {
-      setSelectedIsNew(curr => {
-        let guid = selectedTemplate.guid?.trim();
-        let exists = templates.find(t => t.guid === guid) != null;
-        return !exists
-      })
-    }, [selectedTemplate]);
-  
-    function SetGuid(template: IEscalationTemplate) { 
-      if(template.guid?.trim()) return;
-      template.guid = uuidv4();
     }
-  
-    function UpdateLabel(value:string) {
-      setSelectedTemplate(curr => {
-        let d = Object.assign({}, curr)
-          d.label = value;
-          return d;
-      });
+    return () => {
+      cancel = true;
     }
-    
-    function UpdateContent(value:string) {
-      setSelectedTemplate(curr => {
-          let d = Object.assign({}, curr)
-          d.value = value;
-          return d;
-      });
-    }
-  
-    function OnTemplateSelect(template:EscalationTemplate) {
-        setSelectedTemplate(template);
-        setOpen(true);
-    }
-  
-    function AddTemplate() {
-      setSelectedTemplate(new EscalationTemplate('', '', ''));
-    }
-  
-    function HandleSaveOrCreate() {
-      if(selectedIsNew) {
-        setTemplates(curr => {
-          return [...curr, Object.assign({}, selectedTemplate)]
-        });
-      } else {
-        setTemplates(curr => {
-          const d = [...curr];
-          const target = d.find(t => t.guid === selectedTemplate.guid)
-          if(!target) return [...d];
-          target.label = selectedTemplate.label;
-          target.value = selectedTemplate.value;
-          return [...d]
-        });
+  }, [user])
+
+  useEffect(() => {
+    if (loading) return;
+    PersistTemplates();
+  }, [templates])
+
+  useEffect(() => {
+    if (templates.every(t => Boolean(t.guid?.trim()))) return;
+    setTemplates(curr => {
+      const d = [...curr];
+      for (const template of d) {
+        template.guid = uuidv4();
       }
-    }
-  
-    function RemoveTemplate(template:EscalationTemplate) {
-      setTemplates( curr => {
+      return d;
+    });
+  }, [templates])
+
+  useEffect(() => {
+    if (selectedTemplate.guid?.trim()) return;
+    setSelectedTemplate(curr => {
+      const d = Object.assign({}, curr);
+      SetGuid(d);
+      return d;
+    });
+  }, [selectedTemplate])
+
+  useEffect(() => {
+    setSelectedIsNew(curr => {
+      let guid = selectedTemplate.guid?.trim();
+      let exists = templates.find(t => t.guid === guid) != null;
+      return !exists
+    })
+  }, [selectedTemplate]);
+
+  function SetGuid(template: IEscalationTemplate) {
+    if (template.guid?.trim()) return;
+    template.guid = uuidv4();
+  }
+
+  function UpdateLabel(value: string) {
+    setSelectedTemplate(curr => {
+      let d = Object.assign({}, curr)
+      d.label = value;
+      return d;
+    });
+  }
+
+  function UpdateContent(value: string) {
+    setSelectedTemplate(curr => {
+      let d = Object.assign({}, curr)
+      d.value = value;
+      return d;
+    });
+  }
+
+  function OnTemplateSelect(template: EscalationTemplate) {
+    setSelectedTemplate(template);
+    setOpen(true);
+  }
+
+  function AddTemplate() {
+    setSelectedTemplate(new EscalationTemplate('', '', ''));
+  }
+
+  function HandleSaveOrCreate() {
+    if (selectedIsNew) {
+      setTemplates(curr => {
+        return [...curr, Object.assign({}, selectedTemplate)]
+      });
+    } else {
+      setTemplates(curr => {
         const d = [...curr];
-        let idx = d.findIndex(t => t.guid === template.guid);
-        if(idx >= 0)
-          d.splice(idx, 1);
-        return d;
+        const target = d.find(t => t.guid === selectedTemplate.guid)
+        if (!target) return [...d];
+        target.label = selectedTemplate.label;
+        target.value = selectedTemplate.value;
+        return [...d]
       });
     }
-  
-    return (
-        
-        <Dialog>
-          <div>
-            <div className='flex flex-wrap justify-start mt-5'>
-              {
-                loading ?
-                [32, 40, 64, 36, 40, 32, 64, 32, 36, 40, 64, 32].map((x,i) => {
-                    return <Skeleton key={i} className={`h-10 w-${x} mb-3 mr-3`} />
-                  })
-                : 
-                <>
-                  {templates.map( (t,i) => (
-                    <ContextMenu key={i}>
-                        <DialogTrigger asChild>
-                          <ContextMenuTrigger asChild>
-                            <Button onClick={e=>OnTemplateSelect(t)} className="mb-3 mr-3" variant={"secondary"}>{t.label}</Button>
-                          </ContextMenuTrigger>
-                        </DialogTrigger>
-                      <TemplateContextContent template={t} OnRemove={RemoveTemplate} />
-                    </ContextMenu>
-                  ))}
-                  <DialogTrigger asChild>
-                      <Button variant={"ghost"} onClick={e=>AddTemplate()}><Icons.plus className="mr-2 h-4 w-5" />New Template</Button>
-                  </DialogTrigger>
-                </>
-                }
-            </div>
-          </div>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>{selectedIsNew ? 'Create Template' : 'Edit Template'}</DialogTitle>
-              <DialogDescription>
-                {selectedIsNew ? "Create a new redemption template. Label and content must be provided" : "Make changes to your redemption template here. Click save when you're done." }
-              </DialogDescription>
-            </DialogHeader>
-            <TemplateForm template={selectedTemplate} OnLabelChange={UpdateLabel} OnValueChange={UpdateContent} />
-            <DialogFooter>
-              <DialogTrigger asChild>
-                <Button type="button" onClick={HandleSaveOrCreate} > {selectedIsNew ? 'Create' : 'Save changes'}</Button>
-              </DialogTrigger>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-    )
+  }
+
+  function RemoveTemplate(template: EscalationTemplate) {
+    setTemplates(curr => {
+      const d = [...curr];
+      let idx = d.findIndex(t => t.guid === template.guid);
+      if (idx >= 0)
+        d.splice(idx, 1);
+      return d;
+    });
+  }
+
+  return (
+
+    <Dialog>
+      <div>
+        <div className='flex flex-wrap justify-start mt-5'>
+          {
+            loading ?
+              [32, 40, 64, 36, 40, 32, 64, 32, 36, 40, 64, 32].map((x, i) => {
+                return <Skeleton key={i} className={`h-10 w-${x} mb-3 mr-3`} />
+              })
+              :
+              <>
+                {templates.map((t, i) => (
+                  <ContextMenu key={i}>
+                    <DialogTrigger asChild>
+                      <ContextMenuTrigger asChild>
+                        <Button onClick={e => OnTemplateSelect(t)} className="mb-3 mr-3" variant={"secondary"}>{t.label}</Button>
+                      </ContextMenuTrigger>
+                    </DialogTrigger>
+                    <TemplateContextContent template={t} OnRemove={RemoveTemplate} />
+                  </ContextMenu>
+                ))}
+                <DialogTrigger asChild>
+                  <Button variant={"ghost"} onClick={e => AddTemplate()}><Icons.plus className="mr-2 h-4 w-5" />New Template</Button>
+                </DialogTrigger>
+              </>
+          }
+        </div>
+      </div>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{selectedIsNew ? 'Create Template' : 'Edit Template'}</DialogTitle>
+          <DialogDescription>
+            {selectedIsNew ? "Create a new redemption template. Label and content must be provided" : "Make changes to your redemption template here. Click save when you're done."}
+          </DialogDescription>
+        </DialogHeader>
+        <TemplateForm template={selectedTemplate} OnLabelChange={UpdateLabel} OnValueChange={UpdateContent} />
+        <DialogFooter>
+          <DialogTrigger asChild>
+            <Button type="button" onClick={HandleSaveOrCreate} > {selectedIsNew ? 'Create' : 'Save changes'}</Button>
+          </DialogTrigger>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
 }
