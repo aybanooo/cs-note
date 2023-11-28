e61a45d6e5e942e4c43f838e0af9a2c45e40e112
Ivan <djohnmarkivan@yahoo.com>
2023-11-28 10:27:10 +0800

    feat: firestore export and import

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import { Note, NoteContent } from "@/types/note"
 import { EscalationTemplate } from "@/types/escalationTemplate";
 import { debug } from "console";
 
-import fs from 'fs'
+import * as fsClient from '@/lib/firebase/clientApp'
 
 const storageKey = 'csnote-notes';
 const storageKey_pending = 'csnote-pending';
@@ -58,20 +58,43 @@ export type ExportableData = {
 }
 
 
-export function GetAllData() {
-    let data:ExportableData = {
-        notes: GetStoredNotes(),
-        pending: GetPendings(),
-        setup: GetSetups(),
-        templates: GetRedemptionTemplates()
-    };
-    return data;
+export async function GetAllData() {
+    if(fsClient.IsLoggedIn()) {
+
+        let fireNotes = await fsClient.GetNotes();
+        let firePendings = await fsClient.GetPendings();
+        let fireSetups = await fsClient.GetSetups();
+        let fireTemplates = await fsClient.GetTemplates();
+
+        let data:ExportableData = {
+            notes: fireNotes,
+            pending: firePendings,
+            setup: fireSetups,
+            templates: fireTemplates
+        };
+        return data;
+    } else {
+        let data:ExportableData = {
+            notes: GetStoredNotes(),
+            pending: GetPendings(),
+            setup: GetSetups(),
+            templates: GetRedemptionTemplates()
+        };
+        return data;
+    }
 }
 
 export function ImportData(data:ExportableData) {
     let s = (x:any) => JSON.stringify(x);
-    localStorage.setItem(storageKeys.notes, s(data.notes));
-    localStorage.setItem(storageKeys.pending, s(data.pending));
-    localStorage.setItem(storageKeys.setup, s(data.setup));
-    localStorage.setItem(storageKeys.templates, s(data.templates));
+    if(fsClient.IsLoggedIn()) {
+        fsClient.SaveNotes(data.notes);
+        fsClient.SavePendings(data.pending);
+        fsClient.SaveSetups(data.setup);
+        fsClient.SaveTemplates(data.templates);
+    } else {
+        localStorage.setItem(storageKeys.notes, s(data.notes));
+        localStorage.setItem(storageKeys.pending, s(data.pending));
+        localStorage.setItem(storageKeys.setup, s(data.setup));
+        localStorage.setItem(storageKeys.templates, s(data.templates));
+    }
 }
